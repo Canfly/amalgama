cf80b657e9636a1b0b95f283b1f92782f10d80f8
Adiom Timur <adiom@canfly.org>
2014-10-02 02:44:20 +0400

    +cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,77 +1,20 @@
-require "bundler/capistrano"
-require "rvm/capistrano"
-require 'sidekiq/capistrano'
+set :application, 'amalgama'
+set :repo_url, 'git@github.com:Canfly/amalgama.git'
 
-server "95.85.4.75", :web, :app, :db, primary: true
+set :deploy_to, '/home/hustlr/amalgama'
 
-set :application, "amalgama"
-set :user, "hustlr"
-set :port, 28
-set :deploy_to, "/home/#{user}/#{application}"
-set :deploy_via, :checkout
-set :use_sudo, true
-
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-set :repository,  "git@github.com:Canfly/amalgama.git"
-set :branch, "master"
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
-
-#run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#set :linked_files, %w{config/database.yml}
+#set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/conf.d/#{application}.conf"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.yml.example"), "#{shared_path}/config/database.yml"
-    puts "Now edit #{shared_path}/config/database.yml and add your username and password"
-  end
-  after "deploy:setup", "deploy:setup_config"
-
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
-
-  desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
-    end
-  end
-  before "deploy", "deploy:check_revision"
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
 end
-
-#role :web, "canfly.org"                          # Your HTTP server, Apache/etc
-#role :app, "amalgama"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
